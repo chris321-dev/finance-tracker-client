11fc0965acfdb9040eddb46dcb270e81e6466759
Christian Okafor <okaforchristian08@gmail.com>
2024-09-26 10:13:29 +0100

    Update financial-record-context.tsx

--- src/contexts/financial-record-context.tsx
+++ src/contexts/financial-record-context.tsx
@@ -33,10 +33,7 @@ export const FinancialRecordsProvider = ({
 
     const fectRecords = async () => {
         if (!user) return
-        // const response = await fetch(`http://localhost:3001/financial-records/getAllByUserID/${user.id}`)
-        const response = await fetch(`https://finance-tracker-server-three.vercel.app/financial-records/getAllByUserID/${user.id}`)
-        
-        
+        const response = await fetch(`https://finance-tracker-server-dusky.vercel.app/financial-records/getAllByUserID/${user.id}`)
 
         if (response.ok) {
             const records = await response.json()
@@ -50,7 +47,7 @@ export const FinancialRecordsProvider = ({
     }, [user])
 
     const addRecord = async (record: FinancialRecord) => {
-       const response = await fetch("https://finance-tracker-server-three.vercel.app/financial-records", {
+       const response = await fetch("https://finance-tracker-server-dusky.vercel.app/financial-records", {
             method: "POST",
             body: JSON.stringify(record),
             headers: {
@@ -67,7 +64,7 @@ export const FinancialRecordsProvider = ({
     }
 
     const updateRecord = async (id: string, newRecord: FinancialRecord) => {
-        const response = await fetch(`https://finance-tracker-server-three.vercel.app/${id}`, {
+        const response = await fetch(`https://finance-tracker-server-dusky.vercel.app/financial-records/${id}`, {
              method: "PUT",
              body: JSON.stringify(newRecord),
              headers: {
@@ -91,7 +88,7 @@ export const FinancialRecordsProvider = ({
      }
 
      const deleteRecord = async (id: string) => {
-        const response = await fetch(`https://finance-tracker-server-three.vercel.app/financial-records/${id}`, {
+        const response = await fetch(`https://finance-tracker-server-dusky.vercel.app/financial-records/${id}`, {
             method: "DELETE",
         })
 
